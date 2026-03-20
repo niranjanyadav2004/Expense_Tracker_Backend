57a18edbefda630b40120affe2ca0bb892d3fb5e
niranjanyadav2004 <niranjanyadav8805@gmail.com>
2026-03-20 11:38:22 +0530

    Updated code

--- src/main/java/com/niranjan/config/SecurityConfig.java
+++ src/main/java/com/niranjan/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.niranjan.config;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -36,6 +37,9 @@ public class SecurityConfig {
 	 
 	@Autowired
 	private PasswordEncoder passwordEncoder;
+	
+	@Value("${FRONTEND_URL}")
+	private String url;
 
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -62,7 +66,7 @@ public class SecurityConfig {
 	@Bean
 	public CorsConfigurationSource corsConfigurationSource() {
 	    CorsConfiguration configuration = new CorsConfiguration();
-	    configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+	    configuration.setAllowedOrigins(List.of(url));
 	    configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
 	    configuration.setAllowedHeaders(List.of("*"));
 	    configuration.setAllowCredentials(true);
